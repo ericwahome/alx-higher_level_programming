030edda65f79b5a1cae1a8d9495e2e95c6e5620d
ericwahome <ewahome68@yahoo.com>
2022-09-14 09:26:10 +0000

    Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+Created on 14.09.2022
+@author: Eric Wachira
+"""
+
+
 class Square:
+    """Class Square that has attributes. Instantiation with size
+    Attributes:
+        size (int): The size of the square
+    """
+
     def __init__(self, size):
-        self.__size = size
+        """The __init__ method for Square class
+        Args:
+            size: (:obj: 'int'): A private instance size
+        """
