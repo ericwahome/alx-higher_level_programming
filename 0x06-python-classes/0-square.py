8369e3b6ce42c60602cc2e5da9f0099dbff074d1
ericwahome <ewahome68@yahoo.com>
2022-09-14 09:22:46 +0000

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Created on 14.09.2022
+@author: Eric Wachira
+"""
+
+
 class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
     pass
