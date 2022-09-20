7a64ba40b1a93243cbd1e72df6f75851d0581326
ericwahome <ewahome68@yahoo.com>
2022-09-20 08:20:45 +0000

    More Classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Created on 20.09.2022
+@author: Eric Wachira
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
