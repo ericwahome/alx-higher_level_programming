c1c06b05d79720491684ffcffc0cddce812a1e0a
ericwahome <ewahome68@yahoo.com>
2022-09-28 14:54:52 +0000

    io

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 """
-Contains the clas "Student"
+Created on 28.09.2022
+@author: Eric Wachira
 """
 
 
 class Student:
-    """Representation of a student"""
+    """
+    Student class
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """
+        init method for Student class
+        Attributes:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """returns a dictionary representation of a Student instance"""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """
+        Represents of Student into json format
+        Return:
+            Student class as a json format
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
